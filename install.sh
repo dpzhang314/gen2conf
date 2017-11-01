73c468b2b4718ba6bd45298310e0bdfc6dadb4f6
dpzhang <dpzhang314@163.com>
2017-11-01 13:46:53 +0800

    Add files

---
+++ install.sh
@@ -0,0 +1,15 @@
+# vim
+ln -s ~/.dotfiles/vim ~/.vim
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+
+# for oh-my-zsh
+ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+
+# for config files
+ln -s ~/.dotfiles/config/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/config/gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/config/ssh ~/.ssh
+
+
+
+
